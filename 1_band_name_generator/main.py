36dd7e8a5d7b55f423da3304a03db6fcd119f314
Sumit Singh <sumitsingh@chingari.io>
2022-08-16 12:40:35 +0530

    day 1

---
+++ 1_band_name_generator/main.py
@@ -0,0 +1,6 @@
+print('Welcome to the Brand name Generator')
+
+city = input('which city did you grow up in?\n')
+pet = input('what is the name of your pet?\n')
+
+print('your band name could be ' + city + ' ' + pet)
