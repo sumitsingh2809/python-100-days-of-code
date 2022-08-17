4951e6525d22f9f4d190e511fb06b113d977e415
Sumit Singh <sumitsingh@chingari.io>
2022-08-17 16:24:15 +0530

    code update

--- 2_tip_calculator/main.py
+++ 2_tip_calculator/main.py
@@ -5,5 +5,6 @@ tip = int(
     input('what tip tip would you like to give? 10, 12 or 15? '))
 people = int(input('How many people to split the bill? '))
 
-total = (total_bill + (total_bill * tip / 100)) / people
-print(f'Each person should pay: ${round(total, 2)}')
+total = round((total_bill + (total_bill * tip / 100)) / people, 2)
+final_amount = "{:.2f}".format(total)
+print(f'Each person should pay: ${final_amount}')
